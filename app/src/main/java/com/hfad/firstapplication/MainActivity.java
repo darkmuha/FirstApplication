85f275bac811e767c18b80603914b470447a115f
darkmuha <darkmuha10@gmail.com>
2022-02-14 21:22:15 +0100

    Did the basic design of the first app

--- app/src/main/java/com/hfad/firstapplication/MainActivity.java
+++ app/src/main/java/com/hfad/firstapplication/MainActivity.java
@@ -1,14 +1,27 @@
 package com.hfad.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView dateTimeDisplay;
+    private Calendar calendar;
+    private SimpleDateFormat dateFormat;
+    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateTimeDisplay = (TextView)findViewById(R.id.last_clicked);
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        date = SimpleDateFormat.format(calendar.getTime());
+        dateTimeDisplay.setText(date);
     }
 }
