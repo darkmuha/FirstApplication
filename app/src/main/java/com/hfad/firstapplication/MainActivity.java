b115be46f676154ef7691d61773975901938a925
darkmuha <darkmuha10@gmail.com>
2022-02-24 00:00:32 +0100

    Everything but the Best value is functional

--- app/src/main/java/com/hfad/firstapplication/MainActivity.java
+++ app/src/main/java/com/hfad/firstapplication/MainActivity.java
@@ -9,13 +9,18 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -24,13 +29,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView day_count, best, last_clicked;
     private Button btn;
     private List<Date> listOfDates = new ArrayList<>();
+    private List<Integer> listOfDays = new ArrayList<>();
     private Date date = new Date();
     private SimpleDateFormat sdf = new SimpleDateFormat("d MMM yyyy");
+    private int day_count_int;
+
 
     @Override
     public void onClick(View v) {
         last_clicked.setText(sdf.format(date));
         listOfDates.add(date);
+        try {
+            listOfDays.add(Integer.parseInt(day_count.toString().substring(6)));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        day_count.setText("Days: 0");
     }
 
     @Override
@@ -41,10 +55,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         day_count = findViewById(R.id.day_count);
         last_clicked = findViewById(R.id.last_clicked);
         best = findViewById(R.id.best);
-        btn = findViewById(R.id.btn);
 
+        btn = findViewById(R.id.btn);
         btn.setOnClickListener(this);
 
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    day_count.setText(differenceOfDays());
+//                    best.setText(String.valueOf(Collections.max(listOfDays)));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 1000);
 
+
+    }
+
+    public String differenceOfDays() throws ParseException {
+        if(listOfDates.isEmpty()) {
+           return "Days: 0";
+        }
+        long diff;
+        diff = date.getTime() - listOfDates.get(listOfDates.size()-1).getTime();
+
+        return ("Days: " + (TimeUnit.MILLISECONDS.toDays(diff)));
     }
 }
