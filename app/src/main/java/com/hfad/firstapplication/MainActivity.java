326168f03692a2b0111d364fead5d3467055c5fd
darkmuha <darkmuha10@gmail.com>
2022-02-23 19:23:41 +0100

    added some new visuals, should be the final edit when it comes to what the app looks like (might change it later)

--- app/src/main/java/com/hfad/firstapplication/MainActivity.java
+++ app/src/main/java/com/hfad/firstapplication/MainActivity.java
@@ -28,6 +28,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
 }
